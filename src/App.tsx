74ff5aa967ed2aed751800836e2792a563156b14
Gustavo <gus.inacio@hotmail.com>
2022-05-20 12:00:18 -0300

    feat: Initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface ITodo {
+  name: string;
+  status: string;
+}
+
 function App() {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const response = await fetch('http://localhost:3001/todo');
+      const json = await response.json();
+      setTodos(json);
+    }
+    getTodos();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo list</h1>
       </header>
+      <main>
+        <div>
+          <ul>
+            {todos.map((todoItem) => (
+              <li>
+                <span>
+                  {todoItem.name}
+                  {todoItem.status}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
